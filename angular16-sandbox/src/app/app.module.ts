4346df92cb2771263038b3bb17f04a88b8fe06ff
SimonS2019 <shentiexiong@gmail.com>
2024-12-09 11:24:43 +1300

    Add HomeComponent and configure routing for the home page

--- angular16-sandbox/src/app/app.module.ts
+++ angular16-sandbox/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // Import this module
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component'; // Import this module
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
