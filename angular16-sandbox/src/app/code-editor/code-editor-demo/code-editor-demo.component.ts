a7e79365b53bb9073103a8ed818e5a52f1708db1
SimonS2019 <shentiexiong@gmail.com>
2024-12-13 10:34:52 +1300

    Enhance CodeEditorDemo with session storage functionality and improved UI text

--- angular16-sandbox/src/app/code-editor/code-editor-demo/code-editor-demo.component.ts
+++ angular16-sandbox/src/app/code-editor/code-editor-demo/code-editor-demo.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import 'brace';
 import 'brace/mode/text';
 import 'brace/theme/github';
@@ -8,11 +8,23 @@ import 'brace/theme/github';
   templateUrl: './code-editor-demo.component.html',
   styleUrls: ['./code-editor-demo.component.css']
 })
-export class CodeEditorDemoComponent {
+export class CodeEditorDemoComponent implements OnInit {
   config = {
     readOnly: false,
     printMargin: 0, // Change this to a number
     fontSize: 14
   };
   value = 'console.log("Hello, world!");'; // Initialize the value property with some default code
+
+  ngOnInit() {
+    const savedValue = sessionStorage.getItem('editorContent');
+    if (savedValue) {
+      this.value = savedValue;
+    }
+  }
+
+  saveToSessionStorage() {
+    sessionStorage.setItem('editorContent', this.value);
+    alert('Content saved to session storage!');
+  }
 }
