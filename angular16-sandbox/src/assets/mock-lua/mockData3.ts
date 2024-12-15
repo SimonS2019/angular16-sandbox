2c48c0aa06d5940341abe4abf4c2346192bf9d85
SimonS2019 <shentiexiong@gmail.com>
2024-12-16 09:46:40 +1300

    Update mockData3 to include email dispatching functionality and script importing

--- angular16-sandbox/src/assets/mock-lua/mockData3.ts
+++ angular16-sandbox/src/assets/mock-lua/mockData3.ts
@@ -1,30 +1,37 @@
 export const mockData3 = {
   value: `
-    -- Complex Lua script example
+    require('MotivIO')
+    require('Site')
+    require('Facts');
 
-    -- Define a table with some data
-    local person = {
-      name = "John Doe",
-      age = 30,
-      hobbies = {"reading", "gaming", "coding"}
-    }
-
-    -- Function to print person details
-    function printPersonDetails(p)
-      print("Name: " .. p.name)
-      print("Age: " .. p.age)
-      print("Hobbies: " .. table.concat(p.hobbies, ", "))
+    function importScript(guid_string)
+       script = MotivIO.LoadExternalScript(guid_string) 
+       pcall_result, handle, error_msg = pcall(load, script)
+       if((pcall_result == true) and (handle ~= nil)) then
+          handle()
+          return true
+       end
+       error('Script: ' .. guid_string .. ' failed to run with error: ' .. error_msg)
+       return false
     end
 
-    -- Function to add a hobby
-    function addHobby(p, hobby)
-      table.insert(p.hobbies, hobby)
+    importScript('c65a0873-165e-41a4-99e0-aa330554085b') -- LIB - SendGridV2
+
+    local email_dispatcher = EMAIL():newDispatcher() -- Creating the email object
+
+    function sendEmail(to, body, subject)
+        
+        from = "noreply@shapetechnology.co.nz"
+        body_type = "text/html"
+        if(subject == nil) then 
+            subject = "Shape Technology System Alert"
+        end
+         
+        email_dispatcher:NewMailAndSend(to, from, subject, body, body_type)
+        
     end
 
-    -- Add a new hobby
-    addHobby(person, "hiking")
-
-    -- Print the updated person details
-    printPersonDetails(person)
+    local body = "Here's some stuff for you..."
+    sendEmail("marco.vandermerwe@shape.co.nz", body,"Hello I'm a Test Alarm")
   `
 };
