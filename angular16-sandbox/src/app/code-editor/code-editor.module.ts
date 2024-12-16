6f05caec4e0b71322a6e4607e32c7c68645ff13b
SimonS2019 <shentiexiong@gmail.com>
2024-12-16 14:34:22 +1300

    Refactor CodeEditor module and routing to streamline ACE configuration

--- angular16-sandbox/src/app/code-editor/code-editor.module.ts
+++ angular16-sandbox/src/app/code-editor/code-editor.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { CodeEditorRoutingModule } from './code-editor-routing.module';
 import { CodeEditorDemoComponent } from './code-editor-demo/code-editor-demo.component';
+import { ACE_CONFIG, AceConfigInterface, AceModule } from 'ngx-ace-wrapper';
 
-
+const DEFAULT_ACE_CONFIG: AceConfigInterface = {
+  tabSize: 2,
+};
 @NgModule({
-  declarations: [
-    CodeEditorDemoComponent
+  declarations: [CodeEditorDemoComponent],
+  imports: [CommonModule, CodeEditorRoutingModule, AceModule],
+  providers: [
+    {
+      provide: ACE_CONFIG,
+      useValue: DEFAULT_ACE_CONFIG,
+    },
   ],
-  imports: [
-    CommonModule,
-    CodeEditorRoutingModule
-  ]
 })
-export class CodeEditorModule { }
+export class CodeEditorModule {}
