39f439946915d7bf0c7c1d36f01ac04b2a434518
SimonS2019 <shentiexiong@gmail.com>
2025-01-17 09:54:00 +1300

    Refactor CodeEditorDirectiveUsage to use ViewChild for code editor element and implement session storage for saving editor content

--- angular16-sandbox/src/app/code-editor/code-editor-directive-usage/code-editor-directive-usage.component.ts
+++ angular16-sandbox/src/app/code-editor/code-editor-directive-usage/code-editor-directive-usage.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-code-editor-directive-usage',
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./code-editor-directive-usage.component.css'],
 })
 export class CodeEditorDirectiveUsageComponent {
+  @ViewChild('codeEditor', { static: false }) codeEditor!: ElementRef;
+
   config = {
     // readOnly: false,
     // printMargin: 100,
@@ -17,18 +19,23 @@ export class CodeEditorDirectiveUsageComponent {
     showPrintMargin: false,
   };
 
-  value = 'This is a test value for the code editor directive usage component.';
+  data = 'This is a test value for the code editor directive usage component.';
   showSaveMessage = false;
 
-  constructor() {}
+  constructor() {
+    const savedValue = sessionStorage.getItem('editorContent');
+    if (savedValue) {
+      this.data = savedValue;
+    }
+  }
 
   saveToSessionStorage() {
-    sessionStorage.setItem('editorContent', this.value);
+    sessionStorage.setItem('editorContent', this.data);
     this.showSaveMessage = true;
     setTimeout(() => (this.showSaveMessage = false), 2000);
   }
 
-  codeChanged(value: any) {
-    console.log('Code changed:', value);
+  codeChanged(e: any) {
+    console.log('Code changed:', e);
   }
 }
