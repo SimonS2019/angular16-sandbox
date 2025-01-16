22bdb1de12e380853ce1210250d6449e16dcdf01
SimonS2019 <shentiexiong@gmail.com>
2025-01-17 09:32:40 +1300

    Add save functionality to CodeEditorDirectiveUsage with feedback message

--- angular16-sandbox/src/app/code-editor/code-editor-directive-usage/code-editor-directive-usage.component.ts
+++ angular16-sandbox/src/app/code-editor/code-editor-directive-usage/code-editor-directive-usage.component.ts
@@ -3,10 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-code-editor-directive-usage',
   templateUrl: './code-editor-directive-usage.component.html',
-  styleUrls: ['./code-editor-directive-usage.component.css']
+  styleUrls: ['./code-editor-directive-usage.component.css'],
 })
 export class CodeEditorDirectiveUsageComponent {
-
   config = {
     // readOnly: false,
     // printMargin: 100,
@@ -18,10 +17,18 @@ export class CodeEditorDirectiveUsageComponent {
     showPrintMargin: false,
   };
 
+  value = 'This is a test value for the code editor directive usage component.';
+  showSaveMessage = false;
 
-  
+  constructor() {}
+
+  saveToSessionStorage() {
+    sessionStorage.setItem('editorContent', this.value);
+    this.showSaveMessage = true;
+    setTimeout(() => (this.showSaveMessage = false), 2000);
+  }
+
   codeChanged(value: any) {
     console.log('Code changed:', value);
   }
-
 }
